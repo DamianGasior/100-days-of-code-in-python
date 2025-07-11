39108de07d93906e53a1a268b5ed300938b0693c
damian <damgas712@gmail.com>
2025-07-11 21:33:28 +0200

    adding extra changes as per story , see #23

--- src/day_007/2_working_file_for_hangman.py
+++ src/day_007/2_working_file_for_hangman.py
@@ -1,8 +1,12 @@
 import random
 
 from hangman_ascii_art import hangman
+from hangman_logo import logo
+from word_list import word_list
 
-word_list=['aardvark', 'ubuebuue']
+print(logo,'\n')
+
+
 
 # 1. Randomly, choose a word froom the word_list and assign it to  a variable, chosen_word . Then print it
 # 2.Ask the user to guess a letter and assign their answer to a variable called guess. Make a guess lowercase
@@ -12,7 +16,7 @@ word_list=['aardvark', 'ubuebuue']
 #
 
 chosen_word=random.choice(word_list)
-print(chosen_word)
+#print(chosen_word)   commetning the word for the user, for dev and testing purpose please uncomment it
 my_list=[]
 
 for i in range(len(chosen_word)):
@@ -20,6 +24,7 @@ for i in range(len(chosen_word)):
     my_list.append('_')     # creating a list with  the number of underscores equal to the  length of the chosen_word
 
 
+print('\n')
 
 #attempts below will be later used to count the succesfull or failed number of attempts in guessing the letters
 
@@ -31,44 +36,52 @@ failed_attempts=0
 while '_' in my_list and failed_attempts < 6:
     guess=input('\nChoose a letter: ').lower()
 
-#using function enumarate, where I can iterate throug my randomly chosen_word and I can have access to the 'index' and 'value' of the chosen_word
+    if guess not in my_list:
+    #using function enumarate, where I can iterate throug my randomly chosen_word and I can have access to the 'index' and 'value' of the chosen_word
 
-    for index, symbol in enumerate(chosen_word):
+        for index, symbol in enumerate(chosen_word):
+            
+            if guess==symbol :
+                my_list[index]=symbol
+            elif guess!=symbol :
+                if my_list[index]=='_':
+                    my_list[index]='_'
+                else:
+                    continue
+        #creating an empty string, where I will display letters and '_' each time after users guess
+        my_word=''
+
+        #adding all the elements from my_list to the my_word, so that we can print it out in a nice format to the user
+        for i in my_list:
+            my_word+=i
+
+        if guess in my_word:
+            succesfull_attempts+=1
+        else:
+            failed_attempts+=1
+            print(f'\nYou guessed "{guess}", thats not in the word. You lose life.')
+
         
-        if guess==symbol :
-            my_list[index]=symbol
-        elif guess!=symbol :
-            if my_list[index]=='_':
-                my_list[index]='_'
-            else:
-                continue
-    #creating an empty string, where I will display letters and '_' each time after users guess
-    my_word=''
-
-    #adding all the elements from my_list to the my_word, so that we can print it out in a nice format to the user
-    for i in my_list:
-        my_word+=i
-
-    if guess in my_word:
-        succesfull_attempts+=1
-    else:
-        failed_attempts+=1
-
-    if  failed_attempts <6:
-        print(hangman[failed_attempts])
-        print(my_word,'\n')
-    elif failed_attempts == 6:
-        print('You lose')
-        print(hangman[failed_attempts])
+        remaining_lives=6-failed_attempts
         
 
 
-
-
-
+        if  failed_attempts <6 :
+            print(hangman[failed_attempts])
+            print(f'\nYour guessed letters are: {my_word}\n')
+            print(f'\n************************************************** {remaining_lives} out of 6 LIVES LEFT *************************************************************\n')
+        elif failed_attempts == 6:
+            print(hangman[failed_attempts])
+            print(f'\n************************************************** {remaining_lives} out of 6 LIVES LEFT *************************************************************\n')
+            print('\n*************************************************** YOU LOSE ******************************************************')
+        
     
+    # if the  the hosen letter was already guessed, print to the user the below message
+    else:
+        print(f'You guessed a "{guess}" letter, that is already there. You keep your life.Try again')    
+
 
 if '_' not in my_word:
-    print('You won!\n')
+    print('*****************************************************YOU WIN******************************************************\n')
 
 
