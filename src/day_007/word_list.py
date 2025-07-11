8b185a1597114d51ed8ecdaf73d7c68bc87413c4
damian <damgas712@gmail.com>
2025-07-11 16:07:09 +0200

    adding word_list closes #25

---
+++ src/day_007/word_list.py
@@ -0,0 +1,12 @@
+word_list = [
+"apple", "ball", "cat", "dog", "egg", "fish", "goat", "hat", "ice", "jam",
+"kite", "lamp", "moon", "nest", "orange", "pen", "queen", "rain", "sun", "tree",
+"umbrella", "van", "whale", "xray", "yarn", "zoo", "book", "chair", "desk", "door",
+"ear", "face", "gift", "hand", "ink", "joke", "key", "leaf", "milk", "nose",
+"ocean", "pig", "quiz", "rock", "shoe", "tail", "up", "vase", "wall", "box",
+"yellow", "zip", "ant", "bat", "cold", "duck", "eat", "fast", "green", "hill",
+"icecream", "jump", "king", "long", "mud", "night", "open", "play", "quiet", "red",
+"snow", "top", "under", "voice", "water", "axe", "yell", "zebra", "angel", "blanket",
+"cloud", "drum", "elf", "frog", "glove", "hill", "iron", "jelly", "kite", "lamp",
+"mirror", "net", "owl", "pizza", "queen", "rope", "sock", "tent", "unit", "vest"
+]
