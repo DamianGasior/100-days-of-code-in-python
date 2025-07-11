fa84bb36f3ee9e80f131a882b9e0ec561ad99d4c
damian <damgas712@gmail.com>
2025-07-11 22:24:12 +0200

    removing following lines from code , old instructions which were used at the beginnig of the file.

--- src/day_007/2_working_file_for_hangman.py
+++ src/day_007/2_working_file_for_hangman.py
@@ -8,13 +8,6 @@ print(logo,'\n')
 
 
 
-# 1. Randomly, choose a word froom the word_list and assign it to  a variable, chosen_word . Then print it
-# 2.Ask the user to guess a letter and assign their answer to a variable called guess. Make a guess lowercase
-# 3.Check if the letter theuser guessed ( guess) is one of the letters i the chosen_word . Print "Right" if it is, 
-# "Wrong" if its not. 
-#
-#
-
 chosen_word=random.choice(word_list)
 #print(chosen_word)   commetning the word for the user, for dev and testing purpose please uncomment it
 my_list=[]
