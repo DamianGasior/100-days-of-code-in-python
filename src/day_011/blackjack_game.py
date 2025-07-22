3e80ddf4e3d4ad806894b986191d95eb025d8e21
damian <damgas712@gmail.com>
2025-07-23 00:56:45 +0200

    changing the condition , instead of break, applying return users_decision_formatted , see #41

--- src/day_011/blackjack_game.py
+++ src/day_011/blackjack_game.py
@@ -1,9 +1,12 @@
 
+deck_of_cards=[11,2,3,4,5,6,7,8,9,10,10,10,10,11,2,3,4,5,6,7,8,9,10,10,10,10,11,2,3,4,5,6,7,8,9,10,10,10,10,11,2,3,4,5,6,7,8,9,10,10,10,10 ]
+
+
 def question_validaton_function(users_input): 
     while True:
         users_decision_formatted=input(users_input).strip().lower()
         if users_decision_formatted =='y' or users_decision_formatted=='n':
-            break
+            return users_decision_formatted 
         else:
             print('Insert a valid entry')
 
@@ -11,5 +14,6 @@ def question_validaton_function(users_input):
 
 welcome_statement=question_validaton_function("Do you want to play a game of Blackjack ? Type 'y' or 'n' :")
 
-
 continue_game_question=question_validaton_function("Type 'y' to get another card, type 'n' to pass: ")
+
+
