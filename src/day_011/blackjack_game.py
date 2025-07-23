f68a3ec65ab3ad7cb8e2b986faa21677a77d2176
damian <damgas712@gmail.com>
2025-07-23 12:13:11 +0200

    adding the check for the first round for the player if total score equal to 21, see #40

--- src/day_011/blackjack_game.py
+++ src/day_011/blackjack_game.py
@@ -62,7 +62,7 @@ def first_two_dealers_card(updated_deck_of_cards,welcome_statement):
         for i in dealers_deck_of_cards:
             dealers_total_score+=i
         print(f'\nObecna talia kart to {updated_deck_of_cards},\n liczba kart to {len(updated_deck_of_cards)}\n')  
-        print(f'Computers first card: {dealers_deck_of_cards[0]}, current score {dealers_total_score} - this is for Dev purpose only!!') 
+        print(f'Computers first card: {dealers_deck_of_cards[0]}, current score {dealers_total_score} - this is for Dev purpose only!!\n') 
 
         return  dealers_total_score
 
@@ -71,8 +71,8 @@ dealers_total_score=first_two_dealers_card(updated_deck_of_cards,welcome_stateme
 
 
 #napisac warunki, taki plus ze potem ta funkcje bede sobie mogl wykorzystac pozniej juz za kazdym razem jak bede porownywal wyniki
-def round_comparison(dealers_total_score,players_total_score):
-    if players_total_score ==21:
+def round_comparison(dealers_total_score,players_total_score,players_deck_of_cards):
+    if players_total_score ==21 and len(players_deck_of_cards)==2: # check for the first round, if maybe the player did won
          return f'Player has won with :{players_total_score}'
          
     else: 
@@ -90,7 +90,7 @@ def round_comparison(dealers_total_score,players_total_score):
 
        
 
-round_comparison(dealers_total_score,players_total_score)
+round_comparison(dealers_total_score,players_total_score,players_deck_of_cards)
 
 
 continue_game_question=question_validaton_function("Type 'y' to get another card, type 'n' to pass: ")
